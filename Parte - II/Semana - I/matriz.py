bb3ca66143afcb93777dd0733b1a1c354e97473f
WillianFarias <williansousafarias@gmail.com>
2020-09-02 22:01:57 -0300

    Reescrita método de criacao e impressao de matriz

--- Parte - II/Semana - I/matriz.py
+++ Parte - II/Semana - I/matriz.py
@@ -1,12 +1,21 @@
-def cria_matriz(num_linhas, num_colunas, valor):
+def cria_matriz():
+
+  num_linhas = int(input("Linha(s): "))
+  num_colunas = int(input("Coluna(s): "))
   matriz = []
+
   for i in range(num_linhas):
-    #cria a linha i
     linha = []
     for j in range(num_colunas):
+      valor = int(input("Informe o valor da posição [{}][{}]: ".format(i, j)))
       linha.append(valor)
     matriz.append(linha)
   
   return matriz
 
-print(cria_matriz(3, 3, 1))
+
+def imprime_matriz(matriz):
+  for i in range(len(matriz)):
+    print(matriz[i])
+
+imprime_matriz(cria_matriz())
